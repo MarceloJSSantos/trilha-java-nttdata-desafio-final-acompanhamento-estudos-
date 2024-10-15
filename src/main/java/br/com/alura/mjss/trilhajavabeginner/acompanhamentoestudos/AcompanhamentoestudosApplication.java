075f93b89f5a0faaf9e0ae80f5b56139908ccd1c
mssantos <marcelo.silvasantos@emeal.nttdata.com>
2024-10-15 11:50:18 -0300

    Hello World

---
+++ src/main/java/br/com/alura/mjss/trilhajavabeginner/acompanhamentoestudos/AcompanhamentoestudosApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.mjss.trilhajavabeginner.acompanhamentoestudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcompanhamentoestudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcompanhamentoestudosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/mjss/trilhajavabeginner/acompanhamentoestudos/AcompanhamentoestudosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.mjss.trilhajavabeginner.acompanhamentoestudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcompanhamentoestudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
